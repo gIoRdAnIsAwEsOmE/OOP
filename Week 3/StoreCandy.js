ce0db3c1671a75b791d1bfe9a786d77e7259e06e
Giordan Escalona <Gomputer@Giordans-Air.home>
2016-10-13 23:09:46 -0400

    Merge conflicts

--- Week 3/StoreCandy.js
+++ Week 3/StoreCandy.js
@@ -1,5 +1,4 @@
 class StoreCandy {
-<<<<<<< HEAD
     constructor(finderid, candyid, storeid) {
         this._finderid = finderid;
         this._candyid = candyid;
@@ -30,24 +29,6 @@ class StoreCandy {
                 }
             });
         };
-=======
-    constructor() {
-        this.store_ids = [];
-		this.getBounds = (classobject) => {
-			let scquery = new CB.CloudQuery("StoreCandy");
-			if(classobject.constructor.name == "Store"){
-				console.log("Store");
-				scquery.equalsTo(classobject.id);
-				scquery.find({
-					success : (res) => {
-						console.log(res);
-					}	
-				});
-			}else if (classobject.constructor.name == "Candy"){
-				console.log("Currently we cannot find where this candy came from, we are sorry :(");
-			}
-		};
->>>>>>> 2b78be659ea032cc0f588caea881e59d90908a3e
 		this.setBounds = (storeid, candyid) => {
 			this.cloudobject.set("store_id", storeid);
 			this.cloudobject.set("candy_id", candyid);
@@ -58,8 +39,4 @@ class StoreCandy {
         let sc = new CB.CloudObject("StoreCandy");
         return sc;
 		}
-<<<<<<< HEAD
 	}
-=======
-	} 
->>>>>>> 2b78be659ea032cc0f588caea881e59d90908a3e
